402430e08b96b2f6ec4fad9d2ece2a9976f14efb
Vinod Prakash <psvinodprakash@gmail.com>
2020-05-10 20:36:32 +0530

    Correction

--- app/workers/update_user_balance_worker.rb
+++ app/workers/update_user_balance_worker.rb
@@ -5,14 +5,13 @@ class UpdateUserBalanceWorker
 		active_game = Game.find(game_id)
 		# Won bets
 		won_bets = active_game.bet_details.where(bet_number: active_game.thrown_number)
-		amount_from_casino = 0
 		won_bets.each do |won_bet|
 			won_amount = won_bet.amount * PROFIT_RATIO
 			won_bet.user.update_balance(won_amount)
-			amount_from_casino += won_amount
 		end
+		won_amount = won_bets.sum(:amount)
 		won_bets.update_all(bet_status: BetDetail.bet_statuses[:won])
-		active_game.dealer.casino.update_balance(-(amount_from_casino/PROFIT_RATIO))
+		active_game.dealer.casino.update_balance(-won_amount)
 		# Won bets
 
 		# Lost bets
